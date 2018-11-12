b85d0b380730d8a830872bb919801db910a9f76b
朱震庭 <r2qokk@users.noreply.github.com>
2018-11-12 07:32:58 +0000

    Pararell
    - Use `namedtuple`
    - Use `asyncio`

--- download_data.py
+++ download_data.py
@@ -1,104 +1,70 @@
+import asyncio
 import hashlib
-import inspect
-import os
 import json
-import urllib.request
+from collections import namedtuple
+from pathlib import Path
+from urllib.parse import quote, urljoin
+from urllib.request import urlopen
 
-from collections import OrderedDict
-from concurrent.futures import ThreadPoolExecutor, wait
-
-root = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-
-PREFIX = "https://dos.zczc.cz/static/games/bin/"
-DESTINATION = os.path.join(root, 'bin')
-BUF_SIZE = 65536
-THREAD_SIZE = 10
-
-with open(os.path.join(root, 'games.json'), encoding='utf8') as f:
-    content = f.read()
-    game_infos_ordered = json.loads(content, object_pairs_hook=OrderedDict)
-    game_infos = json.loads(content)
+ROOT = Path(__name__).resolve().parent
+INFO = ROOT.joinpath('games.json')
+DESTINATION = ROOT.joinpath('bin')
+BASE = 'https://dos.zczc.cz/static/games/bin/'
+GAME_INFO = namedtuple('GAME_INFO', ['name', 'file_location', 'url', 'hash_value'])
 
 
-def update_json(ordered_dict):
-    with open(os.path.join(root, 'games.json'), encoding='utf8', mode='w') as f:
-        f.write(json.dumps(ordered_dict, indent=2, ensure_ascii=False))
+async def is_download_needed(info):
+    location = info.file_location
+    if location.is_file():
+        sha256 = hashlib.sha256()
+        with open(location, 'rb') as file:
+            sha256.update(file.read())
+        return info.hash_value != sha256.hexdigest()
+    else:
+        return True
 
 
-def generate_sha256(file):
-    """
-    generate file's sha256 checksum
+async def download(info):
+    response = await asyncio.get_event_loop().run_in_executor(None, urlopen, info.url)
+    with open(info.file_location, 'wb', buffering=0) as file:
+        file.write(response.read())
 
-    :param file: file's location
-    :return: sha256 string
-    """
-    sha256 = hashlib.sha256()
-    with open(file, 'rb') as f:
-        while True:
-            data = f.read(BUF_SIZE)
-            if not data:
-                break
-            sha256.update(data)
-    return sha256.hexdigest()
 
-def download(identifier, url, file):
-    print('Downloading {} game file'.format(identifier))
-    urllib.request.urlretrieve(url, file)
+async def unit_of_work(info):
+    name = info.name
+    # TODO curses
+    print('   checking %(name)-20s' % {'name': name}, end='\r')
+    if await is_download_needed(info):
+        print('downloading %(name)-20s' % {'name': name}, end='\r')
+        await download(info)
+        print('done with   %(name)-20s' % {'name': name}, end='\r')
+    else:
+        print('done with   %(name)-20s' % {'name': name}, end='\r')
 
-def main(prefix=PREFIX, destination=DESTINATION):
+
+def main(info):
     """
     check game archives whether exists and their checksum, download from target.
-
-    :return: the list of downloaded file
     """
-    # create folder
-    folder = os.path.isdir(destination)
-    if not folder:
-        os.makedirs(destination)
-
-    executor = ThreadPoolExecutor(max_workers=THREAD_SIZE)
-    all_task = list()
-
-    downloaded = list()
-    for identifier in game_infos['games'].keys():
-        file = os.path.normcase(os.path.join(destination, identifier + '.zip'))
-        url = prefix + urllib.parse.quote(identifier) + '.zip'
-        if os.path.isfile(file) and generate_sha256(file) == game_infos['games'][identifier]['sha256']:
-            print('skip {}'.format(identifier))
-        else:
-            downloaded.append(identifier)
-            task = executor.submit(download, identifier, url, file)
-            all_task.append(task)
-
-    wait(all_task)
-    return downloaded
-
-def files_sha256():
-    """
-    print existing game archives' sha256
-
-    :return: a dict of sha256 string
-    """
-    result = dict()
-    for identifier in game_infos_ordered['games'].keys():
-        file = os.path.normcase(os.path.join(DESTINATION, identifier + '.zip'))
-        if os.path.isfile(file):
-            result[identifier] = generate_sha256(file)
-    return result
-
-
-def update_sha256():
-    """
-    update sha256 to the json
-
-    :return: the updated json
-    """
-    sha256_dict = files_sha256()
-    for identifier, sha256 in sha256_dict.items():
-        game_infos_ordered['games'][identifier]['sha256'] = sha256
-    update_json(game_infos_ordered)
-    return
+    tasks = (unit_of_work(info_) for info_ in info)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.gather(*tasks))
+    loop.close()
+    print('Game on!')
 
 
 if __name__ == '__main__':
-    main()
+    with open(INFO, encoding='utf8') as f:
+        raw_info = json.load(f)
+
+    destination = DESTINATION
+    info = (GAME_INFO(name=name,
+                      file_location=destination.joinpath(name).with_suffix('.zip'),
+                      url=urljoin(BASE, quote(str(Path(name).with_suffix('.zip')))),
+                      hash_value=raw_info['games'][name]['sha256'])
+            for name in raw_info['games'])
+
+    if not destination.is_dir():
+        destination.mkdir()
+
+    main(info)
